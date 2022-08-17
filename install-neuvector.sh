7df29367af2517b4d756450c8e1919fb4d7b34e2
abenginerd <mj@alphabravo.io>
2022-08-17 01:22:54 +0000

    update install script

--- install-neuvector.sh
+++ install-neuvector.sh
@@ -42,8 +42,10 @@ rm ./get_helm.sh  > /dev/null 2>&1
 
 ## Install K3s
 echo "Installing K3s..."
-sudo curl -sfL https://get.k3s.io | sh -  > /dev/null 2>&1
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.23.9+k3s1 sh -  > /dev/null 2>&1
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml  > /dev/null 2>&1
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chmod 0777 ~/.kube/config
 
 ## Wait for K3s to come online
 echo "Waiting for K3s to come online...."
@@ -51,6 +53,7 @@ until [ $(kubectl get nodes|grep Ready | wc -l) = 1 ]; do echo -n "." ; sleep 2;
 
 ## Install Longhorn
 echo "Deploying Longhorn on K3s..."
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}' > /dev/null 2>&1
 helm repo add longhorn https://charts.longhorn.io > /dev/null 2>&1
 helm repo update > /dev/null 2>&1
 helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace > /dev/null 2>&1
